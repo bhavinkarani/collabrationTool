fb670d38c174d6e887e8671ccf6449eb145b531d
Rupinder Singh <rupinder1133@gmail.com>
2017-02-11 15:50:17 +0530

    filed chat scroll issue

--- public/index.js
+++ public/index.js
@@ -50,7 +50,7 @@ $( document ).ready(function(){
           $('.fixedContent').append("<div class='userwrap'><span class='user'>"+user+"</span><span class='messages'>"+$(this).val()+"</span><div class='userwrap'></div>");
           event.preventDefault();
        
-          $(".fixedContent").scrollTop($(".fixedContent").height());
+          $(".fixedContent").scrollTop( $(".fixedContent").prop("scrollHeight") );
           $(this).val('');
       }
       
