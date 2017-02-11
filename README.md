60b42822385a1ba19b41d37b5ae0867aeceb98fe
Rupinder Singh <rupinder1133@gmail.com>
2017-02-11 13:18:34 +0530

    Basic chat

--- README.md
+++
@@ -1,2 +0,0 @@
-# collabrationTool
-onlinecollabrationTool
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var express = require('express');
-var app= express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var io = require('socket.io')(server);
-//var io = require('socket.io').listen(app.listen(port));
-var port=3700;
-server.listen(port);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile( path.join(__dirname,'/public','/index.html') );
 });
 
-io.on('connection', function (socket) {
-  socket.on('chatevent', function (data) {
-    console.log(data);
-    io.emit('chatevent',data);
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message from client', function(data){
+    console.log('message: ' + JSON.stringify( data ) );
+    socket.broadcast.emit( "chat message from server", data );
   });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
