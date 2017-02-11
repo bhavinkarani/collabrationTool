b4af775ea47ec84fc8626a28c6be6ec0bfa97f55
bhavinkarani <bhavinkarani@gmail.com>
2017-02-11 13:36:39 +0530

    adding chat hostory logic

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-
+var chatHistory =[];
+var chunk =5;
 app.get('/', function(req, res){
   res.sendFile( path.join(__dirname,'/public','/index.html') );
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-
+  if(chatHistory.length > chunk){
+  	var slicedArray = fruits.slice(chatHistory.length-chunk, chatHistory.length);
+  	for(var i=0;i<slicedArray.length;i++){
+  		socket.emit( "chat message from server", slicedArray[i] );
+  	}
+  }else{
+	for(var i=0;i<chatHistory.length;i++){
+  		socket.emit( "chat message from server", chatHistory[i] );
+  	}
+  }
   socket.on('chat message from client', function(data){
     console.log('message: ' + JSON.stringify( data ) );
+    chatHistory.push(data);
     socket.broadcast.emit( "chat message from server", data );
   });
 
